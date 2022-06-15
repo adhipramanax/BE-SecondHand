75f7a51ac5e99ff493f88fc065467f19e5bef753
Bagus Budi Wibowo <bagusbudi.0701@gmail.com>
2022-06-15 15:07:40 +0700

    [bagus] create management offer controller

--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -1,11 +1,15 @@
 const router = require('express').Router();
 
 // Import the controllers or other
+const { offerController } = require('../../../app/controllers')
 
 
 // Implement the routes here
 router.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+router.post('/offer', offerController.offerUser)
+router.put('/offer/:id', offerController.updateStatus)
 
 module.exports = router;
