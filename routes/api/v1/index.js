dc8a95c4668f5a4728a5b9f409789f1fc5f9bab9
Lidya Dwi Astuti <lidya.dwi.astuti19@mhs.ubharajaya.ac.id>
2022-06-15 20:12:29 +0700

    create authentication controllers

--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
 
 // Import the controllers or other
+const authenticationController = require('../../../app/controllers/authentication.controller');
+const { register, login } = require('../../../app/requests/auth.validator')
 
 
 // Implement the routes here
-router.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+router.post('/auth/login', login, authenticationController.login);
+router.post('/auth/register', register, authenticationController.register);
 
 module.exports = router;
