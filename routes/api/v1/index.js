ed741d2c9bd072c398087bbac9c25af253252136
Lidya Dwi Astuti <lidya.dwi.astuti19@mhs.ubharajaya.ac.id>
2022-06-16 13:17:02 +0700

    [lidya]feature management profile

--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -7,12 +7,18 @@ const { authJWT } = require('../../../app/middlewares')
 const { register, login } = require('../../../app/requests/auth.validator')
 const { create } = require('../../../app/requests/offer.validator')
 const { offerController } = require('../../../app/controllers')
+const profilController = require('../../../app/controllers/profile.controller')
 
 // Implement the routes here
 router.post('/auth/login', login, authenticationController.login);
 router.post('/auth/register', register, authenticationController.register);
 
-router.post('/offer', [ authJWT, create ], offerController.offerUser)
-router.put('/offer/:id', [ authJWT ], offerController.updateStatus)
+router.post('/offer', [authJWT, create], offerController.offerUser)
+router.put('/offer/:id', [authJWT], offerController.updateStatus)
+
+//profil user
+router.get('/profile', [authJWT], profilController.getAll)
+router.put('/profile/:id', [authJWT], profilController.updateuser)
+
 
 module.exports = router;
