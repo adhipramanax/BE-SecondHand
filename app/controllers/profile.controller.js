4131b427dac76f141e212f230220d3ad9f27d065
Lidya Dwi Astuti <lidya.dwi.astuti19@mhs.ubharajaya.ac.id>
2022-06-16 12:07:42 +0700

    [Lidya] Membuat Profil

---
+++ app/controllers/profile.controller.js
@@ -0,0 +1,40 @@
+require('dotenv').config()
+const { User } = require('../models')
+const responseFormatter = require('../helpers/responseFormatter')
+
+// get all user use class
+class profilController {
+  static async getAll(req, res) {
+    try {
+      const user = await User.findAll()
+      res.status(200).json(responseFormatter.success(user, 'User berhasil ditampilkan!', res.statusCode))
+    } catch (error) {
+      res.status(500).json(responseFormatter.error(null, error.message, res.statusCode))
+    }
+  }
+
+  static async updateuser(req, res) {
+    try {
+      const user = await User.findOne({ where: { id: req.params.id } })
+      if (!user) {
+        res.status(404).json(responseFormatter.error(null, 'User tidak ditemukan!', res.statusCode))
+      }
+      const { name, city, address, phone_number, url_photo } = req.body
+      const newUser = await User.update({
+        name,
+        city,
+        address,
+        phone_number,
+        url_photo
+      }, {
+        where: { id: req.params.id }
+      })
+      res.status(200).json(responseFormatter.success(newUser, 'User berhasil diubah!', res.statusCode))
+    } catch (error) {
+      res.status(500).json(responseFormatter.error(null, error.message, res.statusCode))
+    }
+
+  }
+}
+
+module.exports = profilController
--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -3,10 +3,18 @@ const router = require('express').Router();
 // Import the controllers or other
 const authenticationController = require('../../../app/controllers/authentication.controller');
 const { register, login } = require('../../../app/requests/auth.validator')
+const profilController = require('../../../app/controllers/profile.controller');
+
 
 
 // Implement the routes here
 router.post('/auth/login', login, authenticationController.login);
 router.post('/auth/register', register, authenticationController.register);
+
+
+
+// profil user 
+router.get('/profile', profilController.getAll);
+router.put('/profile/:id', profilController.updateuser);
 
 module.exports = router;
