ef523a85ba135f22b8c2d26e71194f8228297d4d
Bagus Budi Wibowo <bagusbudi.0701@gmail.com>
2022-06-27 22:34:36 +0700

    [bagus] update api endpoint

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,11 +1,13 @@
 const authenticationController = require('./authentication.controller');
 const ProductController = require('./product.controller');
 const offerController = require('./offer.controller');
+const historyTransactionController = require('./historyTransaction.controller');
 const categoryController = require('./category.controller');
 
 module.exports = {
   authenticationController,
   ProductController,
   offerController,
+  historyTransactionController,
   categoryController
 }
