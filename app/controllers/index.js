3ce63646ced879b2c45fcb47073561e4b73babda
PANJI ADHIPRAMANA <panjiadhipramana@gmail.com>
2022-06-28 09:57:50 +0800

    [Panji] deploy test

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,11 +1,13 @@
-const authenticationController = require('./authentication.controller');
-const ProductController = require('./product.controller');
-const offerController = require('./offer.controller');
-const categoryController = require('./category.controller');
+const authenticationController = require("./authentication.controller");
+const ProductController = require("./product.controller");
+const offerController = require("./offer.controller");
+const categoryController = require("./category.controller");
+const historyTransactionController = require("./historyTransaction.controller");
 
 module.exports = {
-  authenticationController,
-  ProductController,
-  offerController,
-  categoryController
-}
+    authenticationController,
+    ProductController,
+    offerController,
+    categoryController,
+    historyTransactionController,
+};
--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -1,15 +1,15 @@
 const router = require("express").Router();
 
 // Import the controllers or other
-const { authJWT, upload } = require('../../../app/middlewares')
-const { register, login } = require('../../../app/requests/auth.validator')
-const { createProduct }  = require('../../../app/requests/product.validator')
-const { create } = require('../../../app/requests/offer.validator')
-const { authenticationController, ProductController, offerController, categoryController } = require('../../../app/controllers')
+const { authJWT, upload } = require("../../../app/middlewares");
+const { register, login } = require("../../../app/requests/auth.validator");
+const { createProduct } = require("../../../app/requests/product.validator");
+const { create } = require("../../../app/requests/offer.validator");
+const { authenticationController, ProductController, offerController, categoryController, historyTransactionController } = require("../../../app/controllers");
 
 // Implement the routes here
-router.post('/auth/login', login, authenticationController.login);
-router.post('/auth/register', register, authenticationController.register);
+router.post("/auth/login", login, authenticationController.login);
+router.post("/auth/register", register, authenticationController.register);
 
 // Management Product
 router.get("/product", ProductController.getAllProduct);
@@ -18,23 +18,22 @@ router.get("/product/filter", ProductController.filterByCategory);
 router.get("/product/:id", ProductController.getProductById);
 
 // Product by seller
-router.post("/seller/product", [authJWT, upload.array('image')], ProductController.createProduct);
-router.get("/seller/product/sold", [ authJWT ], ProductController.getProductSold);
-router.get("/seller/product/offer", [ authJWT ], ProductController.getProductOffered);
-router.get("/seller/product/offer/:id", [ authJWT ], ProductController.getDetailProductOffered);
-router.get("/seller/product/:status", [ authJWT ], ProductController.getProductByStatus);
-router.put("/seller/product/:id", [ authJWT, upload.array('image') ], ProductController.updateProduct);
-router.put("/seller/product/status/:id", [ authJWT ], ProductController.updateStatusProduct);
-router.delete("/seller/product/:id", [ authJWT ], ProductController.deleteProduct);
+router.post("/seller/product", [authJWT, upload.array("image")], ProductController.createProduct);
+router.get("/seller/product/sold", [authJWT], ProductController.getProductSold);
+router.get("/seller/product/offer", [authJWT], ProductController.getProductOffered);
+router.get("/seller/product/offer/:id", [authJWT], ProductController.getDetailProductOffered);
+router.get("/seller/product/:status", [authJWT], ProductController.getProductByStatus);
+router.put("/seller/product/:id", [authJWT, upload.array("image")], ProductController.updateProduct);
+router.put("/seller/product/status/:id", [authJWT], ProductController.updateStatusProduct);
+router.delete("/seller/product/:id", [authJWT], ProductController.deleteProduct);
 
 // Management offer
-router.post('/offer', [ authJWT, create ], offerController.offerUser)
-router.get('/offer/:id', [ authJWT ], offerController.showOfferProduct)
-router.put('/offer/:id', [ authJWT ], offerController.updateStatus)
-router.get('/offer-history/:id', historyTransactionController.offerHistory)
+router.post("/offer", [authJWT, create], offerController.offerUser);
+router.get("/offer/:id", [authJWT], offerController.showOfferProduct);
+router.put("/offer/:id", [authJWT], offerController.updateStatus);
+router.get("/offer-history/:id", historyTransactionController.offerHistory);
 
 // Management Category
-router.get('/categories', categoryController.getAllCategory)
+router.get("/categories", categoryController.getAllCategory);
 
 module.exports = router;
-
