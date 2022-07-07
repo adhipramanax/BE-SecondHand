35fff3e66993156f7748d722cae950782637d0b1
PANJI ADHIPRAMANA <panjiadhipramana@gmail.com>
2022-07-07 22:50:27 +0800

    test

--- app/database/seeders/20220615004948-offer.js
+++ app/database/seeders/20220615004948-offer.js
@@ -1,46 +1,46 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    const timestamp = new Date();
+    async up(queryInterface, Sequelize) {
+        const timestamp = new Date();
 
-    await queryInterface.bulkInsert('Offers', [
-      {
-        offer_price: 100000,
-        offer_status: false,
-        id_user: 1,
-        id_product: 1,
-        createdAt: timestamp,
-        updatedAt: timestamp
-      },
-      {
-        offer_price: 200000,
-        offer_status: true,
-        id_user: 1,
-        id_product: 1,
-        createdAt: timestamp,
-        updatedAt: timestamp
-      },
-      {
-        offer_price: 150000,
-        offer_status: false,
-        id_user: 2,
-        id_product: 2,
-        createdAt: timestamp,
-        updatedAt: timestamp
-      },
-      {
-        offer_price: 200000,
-        offer_status: true,
-        id_user: 3,
-        id_product: 2,
-        createdAt: timestamp,
-        updatedAt: timestamp
-      }
-    ])
-  },
+        await queryInterface.bulkInsert("Offers", [
+            {
+                offer_price: 100000,
+                offer_status: null,
+                id_user: 1,
+                id_product: 1,
+                createdAt: timestamp,
+                updatedAt: timestamp,
+            },
+            {
+                offer_price: 200000,
+                offer_status: null,
+                id_user: 1,
+                id_product: 1,
+                createdAt: timestamp,
+                updatedAt: timestamp,
+            },
+            {
+                offer_price: 150000,
+                offer_status: null,
+                id_user: 2,
+                id_product: 2,
+                createdAt: timestamp,
+                updatedAt: timestamp,
+            },
+            {
+                offer_price: 200000,
+                offer_status: null,
+                id_user: 3,
+                id_product: 2,
+                createdAt: timestamp,
+                updatedAt: timestamp,
+            },
+        ]);
+    },
 
-  async down (queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('Offers', null, {});
-  }
+    async down(queryInterface, Sequelize) {
+        await queryInterface.bulkDelete("Offers", null, {});
+    },
 };
