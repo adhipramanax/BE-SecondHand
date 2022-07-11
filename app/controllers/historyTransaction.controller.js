bff2a4703bc039c255cada22d03e8cebcc9bbc40
Bagus Budi Wibowo <bagusbudi.0701@gmail.com>
2022-07-11 22:47:24 +0700

    update history controller

--- app/controllers/historyTransaction.controller.js
+++ app/controllers/historyTransaction.controller.js
@@ -5,82 +5,99 @@ class historyTransactionController{
   static async offerHistory(req, res){
     try {
 
-      // const histories = await History.findAll({
-      //   attributes: ['id'],
-      //   where: {
-      //     id_user: req.user.id
-      //   },
-      //   include: [{
-      //     model: Offer,
-      //     attributes: ['offer_price', 'offer_status', 'createdAt'],
-      //     include: [{
-      //       model: Product,
-      //       attributes: ['name'],
-      //       include: [{
-      //         model: Product_Gallery,
-      //         attributes: ['url_photo'],
-      //       }]
-      //     }]
-      //   }]
-      // })
-
-      const histories = await History.findAll({
-        where: {
-          id_user: req.user.id
-        },
-      });
+      const { id_product, order = false } = req.query;
+      let histories = null
+      
+      if(order){
+        histories = await History.findAll({
+          order: [
+            ['id', 'DESC']
+          ],
+          where: {
+            id_user: req.user.id
+          },
+        });
+      }else{
+        histories = await History.findAll({
+          where: {
+            id_user: req.user.id
+          },
+        });
+      }
 
       const data = histories.map(async (history) => {
-        const offer = await Offer.findOne({
-          attributes: {
-            exclude: ['createdAt', 'updatedAt']
-          },
-          where: {
-            id: history.id_offer,
-          }
-        })
+        let offer = null
 
-        const product = await Product.findOne({
-          attributes: {
-            exclude: ['createdAt', 'updatedAt']
-          },
-          where: {
-            id: offer.id_product
-          }
-        })
-
-        const galleries = await Product_Gallery.findAll({
-          attributes: ["url_photo"],
-          where: {
-            id_product: product.id
-          }
-        });
-
-        const productDetail = await Detail_Product.findAll({
-          where: {
-            id_product: product.id
-          }
-        });
-
-        const categories = await Category.findAll({
-          attributes: ["name"],
-          where: {
-            id: productDetail.map(detail => detail.id_category)
-          }
-        });
-
-        return {
-          id: history.id,
-          offer,
-          product,
-          categories,
-          galleries
+        if(id_product){
+          offer = await Offer.findOne({
+            attributes: {
+              exclude: ['createdAt', 'updatedAt']
+            },
+            where: {
+              id: history.id_offer,
+              id_product: id_product
+            }
+          })
+        }else{
+          offer = await Offer.findOne({
+            attributes: {
+              exclude: ['createdAt', 'updatedAt']
+            },
+            where: {
+              id: history.id_offer
+            }
+          })
         }
+
+        if(offer){
+          const product = await Product.findOne({
+            attributes: {
+              exclude: ['createdAt', 'updatedAt']
+            },
+            where: {
+              id: offer.id_product
+            }
+          })
+  
+          const galleries = await Product_Gallery.findAll({
+            attributes: ["url_photo"],
+            where: {
+              id_product: product.id
+            }
+          });
+  
+          const productDetail = await Detail_Product.findAll({
+            where: {
+              id_product: product.id
+            }
+          });
+  
+          const categories = await Category.findAll({
+            attributes: ["name"],
+            where: {
+              id: productDetail.map(detail => detail.id_category)
+            }
+          });
+
+          return {
+            id: history.id,
+            offer,
+            product,
+            categories,
+            galleries
+          }
+        }else{
+          return false
+        }
+
       })
 
       const result = await Promise.all(data)
 
-      console.log(result);
+      if(!result[0]){
+        res.status(404).json(responseFormatter.error(null , "Riwayat penawaran tidak ditemukan", res.statusCode))
+        return
+      }
   
       res.status(200).json(responseFormatter.success(result , "Riwayat penawaran berhasil ditemukan", res.statusCode))  
     } catch (error) {
